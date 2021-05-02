23d721b48b9cd0d459dcdda2d9b924a5f9248136
ivangeorgiev97 <georgiev1313@abv.bg>
2021-05-02 18:05:48 +0300

    WIP Add Board and Cell classes and images for o and x cells

---
+++ Tic tac toe fmimasters/Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Tic_tac_toe_fmimasters
+{
+    class Board : AbsoluteLayout
+    {
+        const int COLUMNS = 3;
+        const int ROWS = 3;
+        const int MAX_MOVES = 9;
+
+        public event EventHandler TicTacToeStarted;
+        public event EventHandler<bool> TicTacToeEnded;
+
+        Cell[,] cells = new Cell[ROWS, COLUMNS];
+
+        bool inProgress;
+        bool initialized;
+        int usedCells;
+
+        public Board()
+        {
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLUMNS; col++)
+                {
+                    Cell cell = new Cell(row, col);
+                    cells[row, col] = cell;
+                    Children.Add(cell);
+                    // cell.TileStatusChanged += Cell_CellStatusChanged;
+                }
+            }
+
+            // SizeChanged += Board_SizeChanged;//Събитие което възниква при промяна на размера
+
+            // StartNewGame();
+        }
+
+    }
+}
---
+++ Tic tac toe fmimasters/Cell.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Tic_tac_toe_fmimasters
+{
+    enum CellStatus
+    {
+        Closed,
+        X,
+        O
+    }
+
+    class Cell : Frame
+    {
+        CellStatus cellStatus = CellStatus.Closed;
+        Label whoPlays;
+        Label gameStatus;
+        Image oImage;
+        Image xImage;
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public event EventHandler<CellStatus> CellStatusChanged;
+        public Cell(int row, int col)
+        {
+            Row = row;
+            Col = col;
+
+            BackgroundColor = Color.White;
+            OutlineColor = Color.Gray;
+            Padding = 3;
+
+            whoPlays = new Label
+            {
+                TextColor = Color.White,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center
+            };
+
+            gameStatus = new Label
+            {
+                TextColor = Color.White,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center
+            };
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            oImage = new Image { Source = ImageSource.FromResource("Tic_tac_toe_fmimasters.images.o.png", assembly) };
+            xImage = new Image { Source = ImageSource.FromResource("Tic_tac_toe_fmimasters.images.x.png", assembly) };
+
+
+            TapGestureRecognizer singleTap = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1
+            };
+
+            singleTap.Tapped += SingleTap;
+
+            GestureRecognizers.Add(singleTap);
+        }
+
+        private void SingleTap(object sender, EventArgs e)
+        {
+        }
+
+        internal void reset()
+        {
+            // Status = CellStatus.Closed;
+        }
+
+    }
+}
